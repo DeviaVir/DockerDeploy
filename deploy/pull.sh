810dc73011e5ad388f6ea19157b20af003612086
René v. Sweeden <renedx@users.noreply.github.com>
2016-01-10 18:37:17 +0100

    Update pull.sh

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/usr/bin/docker images | awk '{print $1}' | xargs -L1 /usr/bin/docker pull
+containercount=$(/usr/bin/docker ps -a | awk '$1 != "CONTAINER" { n++ }; END { print n+0 }');
+if [ "$containercount" != "0" ]
+then
+  /usr/bin/docker images | awk '{print $1}' | xargs -L1 /usr/bin/docker pull
+fi
