951952adf1e12df06b673316c66e941b00dd51cc
René v. Sweeden <renedx@users.noreply.github.com>
2016-01-12 19:42:27 +0100

    Update stop.sh

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -2,5 +2,9 @@
 containercount=$(/usr/bin/docker ps -a | awk '$1 != "CONTAINER" { n++ }; END { print n+0 }');
 if [ "$containercount" != "0" ]
 then
+  if [ -f "/opt/hooks/stop" ];
+  then
+    /opt/hooks/stop
+  fi
   /usr/bin/docker stop `/usr/bin/docker ps --no-trunc -aq`
 fi
