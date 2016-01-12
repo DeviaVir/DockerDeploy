856e41f5e1f8ff25b49ce22696f699fbe3f48ed7
René v. Sweeden <renedx@users.noreply.github.com>
2016-01-12 19:43:05 +0100

    Update pull.sh

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -2,6 +2,14 @@
 containercount=$(/usr/bin/docker ps -a | awk '$1 != "CONTAINER" { n++ }; END { print n+0 }');
 if [ "$containercount" != "0" ]
 then
+  if [ -f "/opt/hooks/pull" ];
+  then
+    /opt/hooks/pull
+  fi
   OUTPUT=$(/usr/bin/docker images | awk '{print $1}' | xargs -L1 /usr/bin/docker pull);
+  if [ -f "/opt/hooks/pulled" ];
+  then
+    /opt/hooks/pulled
+  fi
   exit 0;
 fi
