cfa2be7338f471b6bd90fd9ff6096842a841ec46
René v. Sweeden <renedx@users.noreply.github.com>
2016-01-12 19:42:40 +0100

    Update start.sh

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,16 +3,17 @@ containercount=$(/usr/bin/docker ps -a | awk '$1 != "CONTAINER" { n++ }; END { p
 if [ "$containercount" != "0" ]
 then
   /usr/bin/docker rm `/usr/bin/docker ps --no-trunc -aq`
-  if [ -f "/opt/hooks/start" ];
-  then
-    /opt/hooks/start
-  fi
   
   if [ -f "/opt/deploydocker" ];
   then
     OUTPUT=$(/opt/deploydocker);
     echo $OUTPUT;
     exit 0;
+  fi
+  
+  if [ -f "/opt/hooks/start" ];
+  then
+    /opt/hooks/start
   fi
   
   exit 1;
