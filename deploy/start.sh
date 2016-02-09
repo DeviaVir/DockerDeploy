59e2d323195741c98a4bbb6df84b258e426df8b1
René v. Sweeden <renedx@users.noreply.github.com>
2016-02-09 13:10:29 +0100

    Update start.sh

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,13 +2,13 @@
 containercount=$(/usr/bin/docker ps -a | awk '$1 != "CONTAINER" { n++ }; END { print n+0 }');
 if [ "$containercount" != "0" ]
 then
-  /usr/bin/docker rm `/usr/bin/docker ps --no-trunc -aq`
-  
   if [ -f "/opt/deploydocker" ];
   then
     OUTPUT=$(/opt/deploydocker);
     echo $OUTPUT;
   fi
+  
+  /usr/bin/docker rmi $(docker images -f 'dangling=true' -q);
   
   exit 0;
 fi
